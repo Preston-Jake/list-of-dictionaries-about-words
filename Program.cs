7ff687b950625b15b383f22d7ebaa5a3b7b4796b
Jake Preston <jakepreston@Jakes-MacBook-Pro.local>
2019-07-09 10:52:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+namespace c__list_of_dictionaries_about_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a new list
+            var dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+            var excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+            // Add Dictionary to your `dictionaryOfWords` list
+
+            dictionaryOfWords.Add(excitedWord);
+
+            // create another Dictionary and add that to the list
+            var flairWord = new Dictionary<string, string>();
+
+            flairWord.Add("word", "flair");
+            flairWord.Add("definition", "a uniquely attractive quality");
+            flairWord.Add("part of speech", "adjective");
+            flairWord.Add("example sentence", "Dance Storm is proficient in handling specialty restaurants and has a flair for culinary art trends.");
+
+            dictionaryOfWords.Add(flairWord);
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach (var word in dictionaryOfWords)
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> wordData in word)
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+            }
+        }
+    }
+}
